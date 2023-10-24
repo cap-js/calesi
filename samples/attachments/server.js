ab514c4fee101851120cf4ae3e05eebf11bd4e64
Mara Kiefer <mara.kiefer@sap.com>
2023-10-24 22:19:25 +0200

    Update attachments samples

--- samples/attachments/server.js
+++ samples/attachments/server.js
@@ -1,57 +1,25 @@
+const path = require('path')
+const fs = require('fs')
 const cds = require('@sap/cds')
 
-// TODO: This will no longer be required once we couple
-// the expand the attachments type and decouple the /media route
-cds.on('served', async () => {
-    // Upload avatar images into store
-    //const { ObjectStoreService } = cds.services
-    //await ObjectStoreService.uploadBulk()
+cds.on('served', async() => {
 
-    // Add url to show customer avatars in table
-    registerHandler(readHandler)
-    async function readHandler(req, next) {
-        const data = await next()
-        const origin = req.http.req.headers.origin
-        const ref = getEntityRef() //req.target.projection.from.ref[0];
-
-        if (data.length && ref) {
-            data.map((d, i) => {
-                if (d && d.customer && ref) {
-                    const file = `${ref}-${d.customer.ID}.png`
-                    Object.assign(d.customer, {
-                        avatar: `${origin}/media/?file=${file}`
-                    })
-                }
-            })
+    // Add sample data for Customer avatar images
+    await UPSERT.into('ProcessorService.Customers').entries(
+        {
+            ID: '8fc8231b-f6d7-43d1-a7e1-725c8e988d18',
+            avatar_content: fs.readFileSync(path.join(__dirname, 'assets', 'Daniel Watts.png')),
+            avatar_type: 'image/png'
+        },
+        {
+            ID: 'feb04eac-f84f-4232-bd4f-80a178f24a17',
+            avatar_content: fs.readFileSync(path.join(__dirname, 'assets', 'Stormy Weathers.png')),
+            avatar_type: 'image/png'
+        },
+        {
+            ID: '2b87f6ca-28a2-41d6-8c69-ccf16aa6389d',
+            avatar_content: fs.readFileSync(path.join(__dirname, 'assets', 'Sunny Sunshine.png')),
+            avatar_type: 'image/png'
         }
-        return data
-    }
+    )
 })
-
-function registerHandler(readHandler) {
-    Object.values(cds.services)
-        .forEach(s => {
-            Object.values(s.entities).forEach(e => {
-                const elements = e.elements;
-                Object.entries(elements).forEach(([k, v]) => {
-                    if (v['@title'] === "Attachments:Image") {
-                        console.log(`> Registering on READ handler on ${e.name}.${k}`)
-                        s.prepend(() => s.on('READ', readHandler))
-                    }
-                })
-            })
-        })
-}
-
-function getEntityRef() {
-    let ref = []
-    Object.values(cds.entities).forEach(c => {
-        const elements = c.elements;
-        Object.entries(elements).forEach(([k, v]) => {
-            if (v['@title'] === "Attachments:Image" && !v.parent.projection) {
-                ref.push(`${c.name}`)
-            }
-        })
-    })
-    return ref[0]
-}
--- samples/attachments/services.cds
+++ samples/attachments/services.cds
@@ -6,16 +6,13 @@ extend incidents.Customers with {
   avatar: Image;
 }
 
-// Same as in Incidents app, but with extra (image) column
-// for customer avatars
-// See: https://sapui5.hana.ondemand.com/sdk/#/topic/492bc791a7bd41cd9932fdf5d3aa2656.html
 annotate ProcessorService.Incidents with @(
     UI.LineItem : [
         ...up to { Value : title },
         // This adds an 'avatar' image column before customer
         {
             $Type : 'UI.DataField',
-            Value : customer.avatar,
+            Value : customer.avatar_url,
             Label: '{i18n>Avatar}'
         },
         ...
